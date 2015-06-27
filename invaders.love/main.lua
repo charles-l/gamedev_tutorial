047969d810842d8f6aad245a469a454c61654a1e
charles-l <chuckiels2011@gmail.com>
2015-06-27 14:50:36 -0400

    end of episode 3

--- invaders.love/main.lua
+++ invaders.love/main.lua
@@ -1,18 +1,61 @@
+love.graphics.setDefaultFilter('nearest', 'nearest')
+enemy = {}
+enemies_controller = {}
+enemies_controller.enemies = {}
+enemies_controller.image = love.graphics.newImage('enemy.png')
+
+function checkCollisions(enemies, bullets)
+  for i, e in ipairs(enemies) do
+    for _, b in pairs(bullets) do
+      if b.y <= e.y + e.height and b.x > e.x and b.x < e.x + e.width then
+        table.remove(enemies, i)
+      end
+    end
+  end
+end
+
 function love.load()
   player = {}
   player.x = 0
-  player.y = 550
+  player.y = 110
   player.bullets = {}
   player.cooldown = 20
-  player.speed = 10
+  player.speed = 2
+  player.image = love.graphics.newImage('player.png')
+  player.fire_sound = love.audio.newSource('Laser_Shoot2.wav')
   player.fire = function()
     if player.cooldown <= 0 then
+      love.audio.play(player.fire_sound)
       player.cooldown = 20
       bullet = {}
-      bullet.x = player.x + 35
-      bullet.y = player.y
+      bullet.x = player.x + 4
+      bullet.y = player.y + 4
       table.insert(player.bullets, bullet)
     end
+  end
+  enemies_controller:spawnEnemy(0, 0)
+  enemies_controller:spawnEnemy(20, 0)
+end
+
+function enemies_controller:spawnEnemy(x, y)
+  enemy = {}
+  enemy.x = x
+  enemy.y = y
+  enemy.width = 10
+  enemy.height = 10
+  enemy.bullets = {}
+  enemy.cooldown = 20
+  enemy.speed = 2
+  table.insert(self.enemies, enemy)
+end
+
+function enemy:fire()
+  if self.cooldown <= 0 then
+    self.cooldown = 20
+    bullet = {}
+    bullet.x = self.x + 35
+    bullet.y = self.y
+    table.insert(self.bullets, bullet)
   end
 end
 
@@ -29,23 +72,34 @@ function love.update(dt)
     player.fire()
   end
 
+  for _,e in pairs(enemies_controller.enemies) do
+    e.y = e.y + 1
+  end
+
   for i,b in ipairs(player.bullets) do
     if b.y < -10 then
       table.remove(player.bullets, i)
     end
-    b.y = b.y - 10
+    b.y = b.y - 2
   end
+  
+  checkCollisions(enemies_controller.enemies, player.bullets)
 end
 
 function love.draw()
+  love.graphics.scale(5)
   -- draw the player
-  love.graphics.setColor(0, 0, 255)
-  love.graphics.rectangle("fill", player.x, player.y, 80, 20)
-
+  love.graphics.setColor(255, 255, 255)
+  
+  love.graphics.draw(player.image, player.x, player.y)
+  
+  for _,e in pairs(enemies_controller.enemies) do
+    love.graphics.draw(enemies_controller.image, e.x, e.y, 0)
+  end
 
   -- draw bullets
   love.graphics.setColor(255, 255, 255)
   for _,b in pairs(player.bullets) do
-    love.graphics.rectangle("fill", b.x, b.y, 10, 10)
+    love.graphics.rectangle("fill", b.x, b.y, 2, 2)
   end
 end
